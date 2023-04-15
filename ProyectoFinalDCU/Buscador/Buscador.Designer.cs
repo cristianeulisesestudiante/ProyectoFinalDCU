76976b823b892b2a9b74ffd8ad950e614a4edd49
Junior1923 <Junior.musseb@gmail.com>
2023-04-14 23:31:26 -0400

    elvis

---
+++ ProyectoFinalDCU/Buscador/Buscador.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace ProyectoFinalDCU.Buscador
+{
+    partial class Buscador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Buscador));
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            groupBox1 = new GroupBox();
+            pictureBox4 = new PictureBox();
+            groupBox2 = new GroupBox();
+            pictureBox2 = new PictureBox();
+            groupBox3 = new GroupBox();
+            pictureBox5 = new PictureBox();
+            groupBox4 = new GroupBox();
+            pictureBox6 = new PictureBox();
+            groupBox5 = new GroupBox();
+            pictureBox8 = new PictureBox();
+            groupBox6 = new GroupBox();
+            pictureBox9 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Arial Narrow", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(524, 51);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(196, 31);
+            textBox2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(514, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(259, 52);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.White;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(726, 52);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(37, 30);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(0, 192, 192);
+            groupBox1.Controls.Add(pictureBox4);
+            groupBox1.Location = new Point(264, 190);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 119);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Quimica";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.MediumAquamarine;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(0, 22);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(200, 97);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 1;
+            pictureBox4.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(pictureBox2);
+            groupBox2.Location = new Point(30, 190);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 119);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Matematica";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.MediumAquamarine;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, 22);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(200, 97);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(pictureBox5);
+            groupBox3.Location = new Point(500, 190);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(200, 119);
+            groupBox3.TabIndex = 5;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Lengua Española";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.MediumAquamarine;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(0, 22);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(200, 97);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 1;
+            pictureBox5.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(pictureBox6);
+            groupBox4.Location = new Point(39, 361);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(200, 116);
+            groupBox4.TabIndex = 5;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Historia";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.MediumAquamarine;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(0, 19);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(200, 97);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 0;
+            pictureBox6.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(pictureBox8);
+            groupBox5.Location = new Point(500, 361);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(200, 116);
+            groupBox5.TabIndex = 5;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Arte";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.MediumAquamarine;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(0, 19);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(200, 97);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 0;
+            pictureBox8.TabStop = false;
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(pictureBox9);
+            groupBox6.Controls.Add(pictureBox7);
+            groupBox6.Location = new Point(264, 361);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(200, 116);
+            groupBox6.TabIndex = 5;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Contabilidad";
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.MediumAquamarine;
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(0, 19);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(200, 97);
+            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox9.TabIndex = 0;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(0, 22);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(200, 97);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 0;
+            pictureBox7.TabStop = false;
+            // 
+            // Buscador
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 510);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox1);
+            Name = "Buscador";
+            Text = "Buscador";
+            Load += Buscador_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            groupBox6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
+        private GroupBox groupBox6;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox7;
+    }
+}
